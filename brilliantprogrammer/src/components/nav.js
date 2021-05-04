c461b38a8591d97ccf136fbae6d54b9027f57e91
Brillianttyagi <deepanshubhai84@gmail.com>
2021-05-04 13:24:13 +0530

    updated nav

--- brilliantprogrammer/src/components/nav.js
+++ brilliantprogrammer/src/components/nav.js
@@ -13,17 +13,17 @@ class Navb extends Component {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto ">
-                <Nav.Link color="red" className="navlinkh" href="/">Home</Nav.Link>
-                <Nav.Link className="navlinkh" href="/python">Python</Nav.Link>
-                <Nav.Link className="navlinkh" href="/">Big Data</Nav.Link>
+                <Nav.Link id="navlinkh" href="/">Home</Nav.Link>
+                <Nav.Link id="navlinkh" href="/python">Python</Nav.Link>
+                <Nav.Link id="navlinkh" href="/">Big Data</Nav.Link>
                 <NavDropdown className="navlinkh" title="Topics" id="basic-nav-dropdown">
-                  <NavDropdown.Item className="navlinkh" href="#action/3.1">Django</NavDropdown.Item>
-                  <NavDropdown.Item className="navlinkh" href="#action/3.2">Web Development</NavDropdown.Item>
-                  <NavDropdown.Item className="navlinkh" href="#action/3.3">React</NavDropdown.Item>
+                  <NavDropdown.Item id="navlinkh" href="#action/3.1">Django</NavDropdown.Item>
+                  <NavDropdown.Item id="navlinkh" href="#action/3.2">Web Development</NavDropdown.Item>
+                  <NavDropdown.Item id="navlinkh" href="#action/3.3">React</NavDropdown.Item>
                   <NavDropdown.Divider />
-                  <NavDropdown.Item className="navlinkh" href="#action/3.4">Tech</NavDropdown.Item>
+                  <NavDropdown.Item id="navlinkh" href="#action/3.4">Tech</NavDropdown.Item>
                 </NavDropdown>
-                <Nav.Link className="navlinkh" href="/">Write</Nav.Link>
+                <Nav.Link id="navlinkk" href="/">Write</Nav.Link>
               </Nav>
               <Form inline>
                 <FormControl type="text" placeholder="Search Category" className="mr-sm-2" />
