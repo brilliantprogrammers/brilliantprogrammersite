132841b95cacfb3222720a4f596efbf441921df4
Brillianttyagi <deepanshubhai84@gmail.com>
2021-05-04 13:15:44 +0530

    added footer component

---
+++ brilliantprogrammer/src/components/Footer.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { Container,Row,Col } from 'react-bootstrap'
+import './css/Footer.css'
+import {
+    Link
+  } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faLink } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter,faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons"
+
+class Foot extends Component {
+    render() { 
+        return ( 
+            <Container  fluid={true} id="footercon">
+                <Row>
+                <Col xs="6" sm="4">
+                    <div class="col-5 offset-1 col-sm-2">
+                        <h5>Links</h5>
+                        <ul class="list-unstyled">
+                            <li id="navlist_"><Link className="navlink" to="/"><FontAwesomeIcon icon={faLink} />Home</Link></li>
+                            <li id="navlist_"><Link className="navlink" to="/project"><FontAwesomeIcon icon={faLink} />Python</Link></li>
+                            <li id="navlist_"><a className="navlink" href="/"><FontAwesomeIcon icon={faLink} />Django</a></li>
+                            <li id="navlist_"><Link className="navlink" to="/Contact"><FontAwesomeIcon icon={faLink} />Contact Us</Link></li>
+                            <li id="navlist_"><Link className="navlink" to="/About"><FontAwesomeIcon icon={faLink} />About Us</Link></li>
+                        </ul>
+                    </div>
+                </Col>
+                <Col xs="6" sm="4">
+                    <div class="address">
+                        <h5>Contact</h5>
+                        <address>
+                            Uttar pradesh<br/>
+                            India<br/>
+                            <FontAwesomeIcon icon={faEnvelope} />
+                            <a className="navlinkw" href="mailto:deepanshubhai84@gmail.com">Write Article</a>
+                        </address>
+                    </div>
+                </Col>
+                <Col sm="4">
+                    <a id="social_" class="btn btn-social-icon btn-facebook" href="https://twitter.com/imaprogramr"><FontAwesomeIcon icon={faTwitter} size="2x" /></a>
+                    <a id="social_" class="btn btn-social-icon btn-linkedin" href="https://www.instagram.com/ttyagi_boy84/"><FontAwesomeIcon icon={faInstagram} size="2x" /></a>                            <a id="social_" class="btn btn-social-icon btn-youtube" href="https://www.linkedin.com/in/deepanshu-tyagi-355855198/"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
+                    <a id="social_" class="btn btn-social-icon" href="mailto:"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a>
+                </Col>
+                </Row>
+            </Container>
+         );
+    }
+}
+ 
+export default Foot;
