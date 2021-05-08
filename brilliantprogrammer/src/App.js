975aba39f2c204a6a5b2b8c54c6fbb339c5cf848
Brillianttyagi <deepanshubhai84@gmail.com>
2021-05-08 13:27:29 +0530

    added scroll to top component'

--- brilliantprogrammer/src/App.js
+++ brilliantprogrammer/src/App.js
@@ -3,21 +3,25 @@ import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Redirect
 } from "react-router-dom";
 import Home from './components/Home';
 import Foot from './components/Footer';
 import Contact from './components/Contact';
 import About from './components/About';
+import ScrollToTop from './components/scroll'
 function App() {
   return (
     <Router basename={'/brilliantprogrammersite'}>
+      <ScrollToTop />
       <div className="App"> 
           <Nav/>
           <Switch>  
             <Route exact path="/" component={Home} />
             <Route path="/contact" component={Contact} />
             <Route path="/about" component={About} />
+            <Redirect to='/'/>
           </Switch>
           <Foot/>
       </div>
